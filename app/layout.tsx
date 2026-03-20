ecd75aca4f4956ba3be38c989db5cf6ba31f972b
Anirudh <your-email@example.com>
2026-03-20 18:36:31 +0530

    Enabled Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import SitePreloader from "./components/site-preloader";
 import { ScrollToTopButton } from "./components/ScrollToTopButton";
 import { Providers } from "./providers";
@@ -27,6 +28,7 @@ export default function RootLayout({
           <ScrollToTopButton />
           {children}
         </Providers>
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
